5a4fd9c4a4efb4bf814be1b66102560258c72961
noahdotpy <noahdotpy@proton.me>
2022-12-08 08:18:08 +1000

    initial commit 🥳

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>ITS ALIVE!!</h1>
+        <h2>{{ count }}</h2>
+        <button @click="count++">Increment</button>
+        <button @click="count--">Decrement</button>
+    </div>
+</template>
+<script setup>
+let count = ref(0)
+</script>
+<style>
+
+</style>
